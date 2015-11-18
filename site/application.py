8a9a1324d326e4b6da9943d9afba6985c7f1b1de
SamKChang <samio54321@gmail.com>
2015-11-18 03:17:40 +0100

    first stable version

--- site/application.py
+++ site/application.py
@@ -3,7 +3,9 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-import ml_engine.converter as cv
+import ml_engine.converter as converter
+import ml_engine.interface as interface
+import ml_engine.dynamic_plot as pt
 
 app = Flask(__name__)
 
@@ -11,42 +13,84 @@ app = Flask(__name__)
 def home():
   return render_template("home.html", mode=0)
 
-@app.route('/results', methods=['POST'])
+@app.route('/', methods=['POST'])
 def inpProcessor():
   if 'crystal' in request.form:
     mode = 0
     # input processing
-    cyl = cv.stringConverter(request.form['crystal'])
+    cyl = converter.stringConverter(request.form['crystal'])
     # output processing
-    out = [cv.outputString(cyl)]
-  elif 'a1' in request.form: 
+    out = interface.outputString([cyl])
+  elif 'target' not in request.form: 
     mode = 1
+    # input processing
     atoms = []
     strList = ['a1', 'a2', 'a3', 'a4']
     for i in range(4):
       flag = strList[i]
       atoms.append(
-        cv.stringConverter(request.form[flag])
+        list(set(converter.stringConverter(request.form[flag])))
       )
-    # input processing
-    cylList = cv.groupConstructor(atoms)
-    out = []
+    cylList = converter.groupConstructor(atoms)
     # output processing
-    for c in cylList:
-      entry = cv.outputString(c)
-      if entry[1]:
-        out.append(entry)
-      else:
-        out = [entry]
-  elif 'include' in request.form:
+    out = interface.outputString(cylList)
+  else:
     mode = 2
     # input processing
-    include = stringConverter(request.form['include'])
-    exclude = stringConverter(request.form['exclude'])
-    element_range = cv.constraint(include, exclude)
-    print element_range
-  
+    atoms = []
+    strList = ['a1', 'a2', 'a3', 'a4']
+    for i in range(4):
+      flag = strList[i]
+      atoms.append(
+        list(set(converter.stringConverter(request.form[flag])))
+      )
+    cylList = atoms
+    target = request.form['target']
+    popsize = request.form['ml_pop']
+    step = request.form['ml_step']
+
+    out = interface.optimizer(target, popsize, cylList, step)
+    out = [['Optimizing crystals with target value: %f'\
+          % float(target)]]
+
+    #pt.plotData()
+
   return render_template("home.html", result=out, mode=mode)
+
+from ml_engine.dynamic_plot import entries as entries
+import json
+from flask import Response
+@app.route('/data', methods=['GET', 'OPTIONS', 'POST'])
+@pt.crossdomain(origin="*", methods=['GET', 'POST'])
+def hello_world():
+    #global entries
+    entries
+    try:
+        modified_since = float(
+          request.headers.get('If-Modified-Since'))
+    except TypeError:
+        modified_since = 0
+
+    new_entries = [e for e in entries\
+                   if e.creation > modified_since]
+    js = json.dumps({'x':[e.x for e in new_entries], 
+                     'y':[e.y for e in new_entries]})
+    resp = Response(js, status=200, mimetype='application/json')
+    print "resp:",
+    print resp
+
+    if new_entries:
+        resp.headers['Last-Modified'] = new_entries[-1].creation
+    elif modified_since:
+        resp.headers['Last-Modified'] = modified_since
+
+    return resp
+
+
+
+
+
+
 
 
 
--- site/ml_engine/converter.py
+++ site/ml_engine/converter.py
@@ -1,48 +1,7 @@
 import itertools
 import re
 import numpy as np
-
-element_dict = {
- 'H': [1,1],
- 'He': [2,2],
- 'Li': [3,3],
- 'Be': [4,4],
- 'B': [5,5],
- 'C': [6,5],
- 'N': [7,5],
- 'O': [8,5],
- 'F': [9,5],
- 'Na': [10,5],
-}
-
-def outputString(atomList):
-  """
-  construct output string
-  calling ML engine from here
-  """
-
-  supported = True
-
-  # crystal string
-  for a in range(len(atomList)):
-    atom = atomList[a]
-    if atom in element_dict:
-      if a == 0:
-        cylStr = atom
-      else:
-        cylStr = cylStr + '-' + atom
-    else:
-      supported = False
-      cylStr = 'Element: %s is not supported yet' % atom
-      break
-
-  # calculate output
-  if supported:
-    E = element_dict[atomList[0]][0]
-  else:
-    E = None
-
-  return [cylStr, E]
+from element_list import element_dict as element_dict
 
 def stringConverter(strInp):
   """
@@ -60,12 +19,8 @@ def stringConverter(strInp):
 def groupConstructor(atom_list):
   return [list(s)	 for s in list(itertools.product(*atom_list))]
 
-import threading
 def constraint(r_list, x_list):
   _list = list(element_dict.iterkeys())
-  e_list = [s for s in _list and x not in x_list]
+  e_list = []
   args = [r_list, x_list]
-
-  threading.Timer(0.5, constraint, *args).start()
-  
 
